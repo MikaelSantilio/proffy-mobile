d40169d1447079607bb45859736fdd03872cae59
MikaelSantilio <mikael.santilio@gmail.com>
2020-08-09 19:50:56 -0300

    adding api request

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
@@ -11,6 +12,24 @@ import styles from './styles';
 
 function Landing() {
     const { navigate } = useNavigation();
+
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data;
+            console.log(response.data);
+
+
+            setTotalConnections(total);
+        }).catch(function (error) {
+            if (error.response) {
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            }
+          })
+    }, []);
 
     function handleNavigationToGiveClassesPage() {
         navigate('GiveClasses');
@@ -49,7 +68,7 @@ function Landing() {
             </View>
 
             <Text style={styles.totalConnections} >
-                Total de 111 conexões já realizadas. {' '}
+                Total de {totalConnections} conexões já realizadas. {' '}
                 <Image source={heartIcon} />
             </Text>
         </View>

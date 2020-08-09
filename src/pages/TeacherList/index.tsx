333ab5a9ad09a62ed65043ae086d9677085929bb
MikaelSantilio <mikael.santilio@gmail.com>
2020-08-09 17:59:00 -0300

    adding filter fields

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Text, TextInput } from 'react-native';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import styles from './styles';
@@ -8,7 +8,33 @@ import styles from './styles';
 function TeacherList() {
     return (
         <View style={styles.container} >
-            <PageHeader title="Proffys disponíveis" />
+            <PageHeader title="Proffys disponíveis" >
+                <View style={styles.searchForm}>
+                    <Text style={styles.label} >Matéria</Text>
+                    <TextInput 
+                        style={styles.input}
+                        placeholder="Qual a matéria?"
+                    />
+
+                    <View style={styles.inputGroup} >
+                        <View style={styles.inputBlock} >
+                            <Text style={styles.label} >Dia da semana</Text>
+                            <TextInput 
+                                style={styles.input}
+                                placeholder="Qual o dia?"
+                            />
+                        </View>
+                        <View style={styles.inputBlock} >
+                            <Text style={styles.label} >Horário</Text>
+                            <TextInput 
+                                style={styles.input}
+                                placeholder="Qual o horário?"
+                            />
+                        </View>
+                    </View>
+                </View>
+                    
+            </PageHeader>
             <ScrollView 
                 style={styles.teacherList}
                 contentContainerStyle={{
